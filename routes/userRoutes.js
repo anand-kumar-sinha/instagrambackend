d4eaba4a1384dfda0507c1f903748b45b53f6f4b
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2024-08-31 16:29:22 +0530

    .

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,8 +3,8 @@ const {registerUser, loginUser} = require("../controller/UserController")
 
 const router = express.Router()
 
-router.route('/register').post(registerUser)
-router.route('/login').post(loginUser)
+router.route('/register').get(registerUser)
+router.route('/login').get(loginUser)
 
 
 module.exports = router
