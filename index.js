e7353a93714f22636348eeb16c7ffab9000afb2a
anandkumarpatel1 <anandkumar7368970125@gmail.com>
2024-08-31 16:45:41 +0530

    .

--- index.js
+++ index.js
@@ -2,18 +2,27 @@ const { connectDataBase } = require("./config/database");
 const dotenv = require("dotenv");
 const express = require("express");
 const cors = require("cors");
-const userRoutes = require('./routes/userRoutes')
+const userRoutes = require("./routes/userRoutes");
 
 const app = express();
 
 app.use(express.json());
 
-app.use(cors({ credentials: true, origin: true }));
+app.use(
+  cors({
+    origin: "*",
+    credentials: true,
+    headers:
+      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+    methods: "GET, POST, PATCH, PUT, POST, DELETE, OPTION",
+  })
+);
+app.use(express.urlencoded({ extended: true }));
 
 dotenv.config();
 connectDataBase();
 
-app.use("/api/v1/users", userRoutes)
+app.use("/api/v1/users", userRoutes);
 app.listen(process.env.PORT, () => {
   console.log(`Server is Started on ${process.env.PORT} port`);
 });
